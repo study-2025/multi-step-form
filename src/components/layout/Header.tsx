5d328c10e5db859afd28e33239253058751975ca
JUNU <bokjunwoo@gmail.com>
2025-02-18 04:42:32 +0900

    feat: Header에 배경 그라디언트 선택 기능 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,31 @@
+import { gradientOptions } from '../../config/colorStyles';
 import { useFormStore } from '../../store/useFormStore';
+import { useGradientStore } from '../../store/useGradientStore';
 import ProgressBar from '../common/ProgressBar';
 
 const Header = () => {
   const { form, currentStep } = useFormStore();
+  const { selectedGradientId, setSelectedGradientId } = useGradientStore();
 
   const totalSteps = Object.keys(form).length;
+
+  if (currentStep === 7) {
+    return (
+      <div className="mt-8 flex gap-4 justify-center">
+        {gradientOptions.map((option) => (
+          <button
+            key={option.id}
+            onClick={() => setSelectedGradientId(option.id)}
+            className={`w-10 h-10 rounded-full border ${
+              selectedGradientId === option.className
+                ? 'border-gray-500'
+                : 'border-gray-300'
+            } ${option.className}`}
+          />
+        ))}
+      </div>
+    );
+  }
 
   if (currentStep < 1 || currentStep > 5) {
     return <header className="py-10"></header>;
