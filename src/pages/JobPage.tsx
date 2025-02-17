08903e009916701975c849435422d5338ddc7a0b
JUNU <bokjunwoo@gmail.com>
2025-02-18 03:08:10 +0900

    feat: JobPage 컴포넌트 구현

---
+++ src/pages/JobPage.tsx
@@ -0,0 +1,20 @@
+import Dropdown from '../components/common/Dropdown';
+import { JOB } from '../components/constant/constant';
+import Main from '../components/layout/Main';
+import { useFormStore } from '../store/useFormStore';
+
+const JobPage = () => {
+  const { form, updateField } = useFormStore();
+
+  return (
+    <Main title="직군을 선택해주세요">
+      <Dropdown
+        items={JOB}
+        selected={JOB.find((item) => item.value === form.job)}
+        onSelect={(item) => updateField('job', item.value)}
+      />
+    </Main>
+  );
+};
+
+export default JobPage;
