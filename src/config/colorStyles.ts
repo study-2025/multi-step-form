4c8baf79bea885f54d7878141c2f66513fe5fad8
JUNU <bokjunwoo@gmail.com>
2025-02-17 13:45:54 +0900

    feat: button colorStyle

---
+++ src/config/colorStyles.ts
@@ -0,0 +1,12 @@
+export const colorStyles: Record<ColorKey, ColorStyle> = {
+  default: {
+    base: 'bg-blue-500 text-white',
+    hover: 'hover:bg-blue-600',
+    disabled: 'bg-gray-200 text-gray-600 cursor-not-allowed',
+  },
+  lightBlue: {
+    base: 'bg-sky-100 text-blue-500',
+    hover: 'hover:bg-blue-200',
+    disabled: 'bg-blue-300 text-white cursor-not-allowed',
+  },
+};
