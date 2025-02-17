7a2b55456096229454e6ff9bfe814675eae10107
JUNU <bokjunwoo@gmail.com>
2025-02-17 13:42:23 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,37 @@
+import { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { colorStyles } from '../../config/colorStyles';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  color?: ColorKey;
+  fullWidth?: boolean;
+  className?: string;
+}
+
+const Button = ({
+  color = 'default',
+  fullWidth = false,
+  children,
+  className,
+  disabled,
+  ...rest
+}: ButtonProps) => {
+  const currentColor = colorStyles[color] || colorStyles.default;
+
+  const btnClass = twMerge(
+    'py-2 px-4 rounded transition-colors duration-200 focus:outline-none text-[15px] h-10',
+    fullWidth ? 'w-full' : '',
+    disabled ? currentColor.disabled : currentColor.base,
+    !disabled && currentColor.hover,
+    className
+  );
+
+  return (
+    <button className={btnClass} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
