d82e5ebce33bc4e2ef4baa2d7675213f1f27c758
D-Sup <andongcelsius@gmail.com>
2025-02-20 09:57:37 +0900

    feat: App.tsx, main.tsx 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-function App() {
+import { useMultiStepFormStore } from "./store/multiStepFormValue"
+
+import ProgressIndicator from "./components/layouts/ProgressIndicator"
+import MainPage from "./pages/MainPage"
+import NameEntryPage from "./pages/NameEntryPage"
+import GenderSelectionPage from "./pages/GenderSelectionPage"
+import HobbySelectionPage from "./pages/HobbySelectionPage"
+import JobSelectionPage from "./pages/JobSelectionPage"
+import IntroEntryPage from "./pages/IntroEntryPage"
+import ConfirmSubmissionPage from "./pages/ConfirmSubmissionPage"
+import StepNavigator from "./components/layouts/StepNavigator"
+
+const App = () => {
+  const { multiStepFormValue } = useMultiStepFormStore()
+
+  const pages = [
+    <MainPage />,
+    <NameEntryPage />,
+    <GenderSelectionPage />,
+    <HobbySelectionPage />,
+    <JobSelectionPage />,
+    <IntroEntryPage />,
+    <ConfirmSubmissionPage />,
+  ]
+
   return (
-    <>
-      <div>다단계 폼 관리 앱 구현 과제</div>
-    </>
-  );
+    <div className="w-screen h-screen bg-accent">
+      <div className="min-w-[375px] max-w-[500px] h-full p-[20px] m-auto bg-white">
+        <header className="h-[10%]">
+          <ProgressIndicator />
+        </header>
+        <main className="h-[70%]">{pages[multiStepFormValue.currentProcess]}</main>
+        <nav className="h-[20%]">
+          <StepNavigator />
+        </nav>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
